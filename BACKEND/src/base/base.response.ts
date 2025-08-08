75efded6bda2f637b880b8b5adc80f10b93c48f7
hai57 <hoanghaiick28@gmail.com>
2025-08-08 11:22:58 +0700

    update base response

--- BACKEND/src/base/base.response.ts
+++ BACKEND/src/base/base.response.ts
@@ -1,4 +1,4 @@
-import type { BaseResponseType } from "src/types/reponse";
+import type { BaseResponseType } from "src/types/base-reponse";
 
 export class BaseResponse {
   protected success<T>({ code, message, result }: BaseResponseType<T>): BaseResponseType<T> {
--- BACKEND/src/types/baseResponse.ts
+++
@@ -1,4 +0,0 @@
-export interface BaseResponse<T = any> {
-    message?: string;
-    result?: T;
-}
