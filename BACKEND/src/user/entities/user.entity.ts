cb9361164add92ed0594ba9f82031772a5ab4940
hai57 <hoanghaiick28@gmail.com>
2025-08-09 18:37:25 +0700

    update auth login register, delete login register in user, create jwt authen guard

--- BACKEND/src/user/entities/user.entity.ts
+++ BACKEND/src/user/entities/user.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
 
 @Entity()
 export class User {
@@ -13,4 +13,19 @@ export class User {
 
   @Column()
   passwordHash: string;
+
+  @Column({ nullable: true })
+  avatarUrl?: string;
+
+  @Column({ default: true })
+  isActive: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+  
+  @Column({ nullable: true })
+  refreshTokenHash?: string;
 }
