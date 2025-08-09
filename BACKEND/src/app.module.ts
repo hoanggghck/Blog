d83902704b2662b52375c9ed6647602a9c6469d0
hai57 <hoanghaiick28@gmail.com>
2025-08-09 09:52:04 +0700

    update config

--- BACKEND/src/app.module.ts
+++ BACKEND/src/app.module.ts
@@ -6,18 +6,27 @@ import { RequestService } from './request.service';
 import { AuthenticaitonMiddleware } from './middleware/authentication.middlewares';
 import { RateLimiterMiddleware } from './middleware/rate-limiter.middleware';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST || 'localhost',
-      port: Number(process.env.DB_PORT) || 5432,
-      username: process.env.DB_USER || 'postgres',
-      password: process.env.DB_PASS || '123456',
-      database: process.env.DB_NAME || 'blogdb',
-      entities: [__dirname + '/**/entities/*.entity{.ts,.js}'], // load tất cả entity
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true, // để mọi module đều dùng được
+      envFilePath: '.env', 
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        username: config.get<string>('DB_USER'),
+        password: config.get<string>('DB_PASS'),
+        database: config.get<string>('DB_NAME'),
+        entities: [__dirname + '/**/entities/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
     }),
     UserModule],
   controllers: [AppController],
