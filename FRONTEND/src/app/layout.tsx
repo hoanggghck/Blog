7ccf6e4140d93f2d32e4ffa01d787f218eafc5a3
Pham Duy Hoang <97928971+kng-duyhoang@users.noreply.github.com>
2025-08-21 15:13:33 +0700

    update

--- FRONTEND/src/app/layout.tsx
+++ FRONTEND/src/app/layout.tsx
@@ -2,7 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from 'react-hot-toast';
 import type { Metadata } from "next";
 
-import "./globals.css";
+import "../styles/index.css";
 import ReactQueryProvider from "@/provider/reactProvider";
 const geistSans = Geist({
   variable: "--font-geist-sans",
