58a4fdfb618615322196090d444d0a92fc35c0b8
Pham Duy Hoang <97928971+kng-duyhoang@users.noreply.github.com>
2025-08-28 21:26:33 +0700

    update

--- BACKEND/src/jwt.module.ts
+++ BACKEND/src/jwt.module.ts
@@ -12,7 +12,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
             inject: [ConfigService],
             useFactory: async (configService: ConfigService) => ({
                 secret: configService.get<string>('JWT_SECRET'),
-                signOptions: { expiresIn: '1m' },
+                signOptions: { expiresIn: '60m' },
             }),
         }),
     ],
